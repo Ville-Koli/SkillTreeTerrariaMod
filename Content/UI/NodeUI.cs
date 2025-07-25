5ea09f9e031383f39f263eefd1e96e7b44a65193
VK <ville.koli.wr@gmail.com>
2025-07-25 20:35:44 +0300

    added documentation to nodeUI

--- Content/UI/NodeUI.cs
+++ Content/UI/NodeUI.cs
@@ -47,112 +47,21 @@ namespace Runeforge.Content.UI
 			id = global_id;
 			global_id++;
 		}
+		/**
+		<summary> 
+		<para>Function that draws the node's image on the panel as active or not depending on activity </para>
+		</summary>
+		**/
 		public override void Draw(SpriteBatch spriteBatch)
 		{
 			spriteBatch.Draw(active ? active_node_image.Value : inactive_node_image.Value, node_image.GetDimensions().Position(), null, Color.White, 0f, Vector2.Zero, SkillTreePanel.zoom, SpriteEffects.None, 0f);
-        }
-		public override string ToString()
-		{
-			//                                             NodeID | Type   | Connections                     | Description
-			// to string method generates a string such as NodeID | TypeID | {(NodeID, NodeID, active), ...} | description
-			StringBuilder nodeString = new StringBuilder();
-			nodeString.Append($"{id} | {type} | ");
-			nodeString.Append('{');
-			foreach (var conn in connections)
-			{
-				nodeString.Append(conn);
-				nodeString.Append(',');
-			}
-			nodeString.Append("} | ");
-			nodeString.Append($"{description}");
-			return nodeString.ToString();
 		}
-		public bool GetNodeActivity()
-		{
-			return active;
-		}
-		public StatBlock GetStatBlock()
-		{
-			return statBlock;
-		}
-		public void SetStatBlock(StatBlock statBlock)
-		{
-			this.statBlock = statBlock;
-		}
-		public string GetDescription()
-		{
-			return description;
-		}
-		public bool CanNodeBeDeActivated(List<ConnectionUI> activeNeighbours)
-		{
-			if (!active) return false;
-			bool deactivation = true;
-			active = false;
-			ModContent.GetInstance<Runeforge>().Logger.Info("MAPPING PATHING! Current connections: " + connections.Count);
-			foreach (var conn in activeNeighbours)
-			{
-				NodeUI node = GetNeighbourNode(conn);
-				deactivation = deactivation && PathingAlgorithms.DoesPossiblePathExistToEmpty(node, new());
-				ModContent.GetInstance<Runeforge>().Logger.Info("Trying neigbhour: " + deactivation);
-			}
-			active = true;
-			return deactivation;
-		}
-
-		public NodeUI GetNeighbourNode(ConnectionUI connection)
-		{
-			if (connection.connectedNodeA.GetID() != id)
-			{
-				return connection.connectedNodeA;
-			}
-			return connection.connectedNodeB;
-		}
-		public static NodeUI GetNeighbourNode(ConnectionUI connection, NodeUI node)
-		{
-			if (connection.connectedNodeA.GetID() != node.GetID())
-			{
-				return connection.connectedNodeA;
-			}
-			return connection.connectedNodeB;
-		}
-
-		public List<ConnectionUI> GetActiveNeigbhourNodeList()
-		{
-			List<ConnectionUI> activeNeighbours = new();
-			foreach (var conn in connections)
-			{
-				NodeUI neigbhour = GetNeighbourNode(conn);
-				bool isEmpty = neigbhour.type == NodeType.Empty;
-
-				if (neigbhour.active || isEmpty)
-				{
-					activeNeighbours.Add(conn);
-				}
-			}
-			return activeNeighbours;
-		}
-
-		public void SetLocation(Vector2 location)
-		{
-			this.location = location;
-		}
-
-		public Vector2 GetLocation()
-		{
-			return location;
-		}
-		public void SetActive()
-		{
-			node_image.SetImage(active_node_image);
-			active = true;
-		}
-
-		public void SetInActive()
-		{
-			node_image.SetImage(inactive_node_image);
-			active = false;
-		}
-
+		/**
+		<summary> 
+		<para>Function, which handles the empty node edge case </para>
+		</summary>
+		<returns>returns a boolean depending on whether node interacted with was type empty or not</returns>
+		**/
 		public bool HandleEmptyNodeCase()
 		{
 			if (!active && type == NodeType.Empty)
@@ -171,11 +80,16 @@ namespace Runeforge.Content.UI
 			}
 			return false;
 		}
+		/**
+		<summary> 
+		<para>Function, which handles left click interaction </para>
+		</summary>
+		**/
 		public override void LeftMouseDown(UIMouseEvent evt)
 		{
 			base.LeftMouseDown(evt);
 			List<ConnectionUI> connections = GetActiveNeigbhourNodeList();
-			bool isLeafNode = connections.Count == 1;
+			bool isLeafNode = connections.Count == 1; // node is a leaf node only and only if the amount of active connections are one
 
 			ModContent.GetInstance<Runeforge>().Logger.Info("START");
 
@@ -211,10 +125,14 @@ namespace Runeforge.Content.UI
 					trigger.DeActivate(statBlock);
 				}
 			}
-			EditHoverOverElement();
+			EditHoverOverElement(); // edit hover over element depending on whether node got activated or deactivated
 			Recalculate();
 		}
-
+		/**
+		<summary> 
+		<para>Function, which initializes the node </para>
+		</summary>
+		**/
 		public override void OnInitialize()
 		{
 			base.OnInitialize();
@@ -224,8 +142,179 @@ namespace Runeforge.Content.UI
 			Height.Set(node_image.Height.Pixels, 0f);
 			Append(node_image);
 		}
+		/**
+		<summary> 
+		<para>Function, which handles entering the element </para>
+		</summary>
+		**/
+		public override void MouseOver(UIMouseEvent evt)
+		{
+			base.MouseOver(evt);
+			if (!mainPanel.isDragging)
+			{
+				mainPanel.isHoveringOverUI = true;
+			}
+			if (type != NodeType.Empty)
+			{
+				EditHoverOverElement();
+			}
+		}
+		/**
+		<summary> 
+		<para>Function, which handles leaving the element </para>
+		</summary>
+		**/
+		public override void MouseOut(UIMouseEvent evt)
+		{
+			base.MouseOver(evt);
+			if (!mainPanel.isDragging)
+			{
+				mainPanel.isHoveringOverUI = false;
+			}
+			hoverOverUI.SetTransparent();
+			hoverOverUI.ChangeDescription("");
+		}
+		/**
+		<summary> 
+		<para>Function, which is ran every in-game tick when panel is open</para>
+		</summary>
+		**/
+		public override void Update(GameTime gameTime)
+		{
+			base.Update(gameTime);
+			// should relocate this to a more optimal location
+			// this just applies scale of the UIImage hitbox and location
+			Width.Set(node_image.Width.Pixels * SkillTreePanel.zoom, 0f);
+			Height.Set(node_image.Height.Pixels * SkillTreePanel.zoom, 0f);
+			location = basePosition * SkillTreePanel.zoom + SkillTreePanel.panOffset;
+			Top.Set(location.Y, 0f);
+			Left.Set(location.X, 0f);
+			Recalculate();
+		}
 
+		/**
+		<summary> 
+		<para>Function, which calculates a boolean on whether node can be deactivated without breaking the
+		active graph.</para>
+		</summary>
+		<param name="activeNeighbours">list of neighbouring elements deduced from connections that are active</param>
+		<returns> a boolean whether node can be deactivated or not </returns>
+		**/
+		public bool CanNodeBeDeActivated(List<ConnectionUI> activeNeighbours)
+		{
+			if (!active) return false;
+			bool deactivation = true;
+			active = false;
+			ModContent.GetInstance<Runeforge>().Logger.Info("MAPPING PATHING! Current connections: " + connections.Count);
+			foreach (var conn in activeNeighbours)
+			{
+				NodeUI node = GetNeighbourNode(conn);
+				deactivation = deactivation && PathingAlgorithms.DoesPossiblePathExistToEmpty(node, new());
+				ModContent.GetInstance<Runeforge>().Logger.Info("Trying neigbhour: " + deactivation);
+			}
+			active = true;
+			return deactivation;
+		}
 
+		/**
+		<summary> 
+		<para>Function, which returns non-self node from connection</para>
+		</summary>
+		<param name="connection">a connection to be analyzed</param>
+		<returns> a neigbhouring node </returns>
+		**/
+		public NodeUI GetNeighbourNode(ConnectionUI connection)
+		{
+			if (connection.connectedNodeA.GetID() != id)
+			{
+				return connection.connectedNodeA;
+			}
+			return connection.connectedNodeB;
+		}
+		/**
+		<summary> 
+		<para>Function, which returns non-self node from connection</para>
+		</summary>
+		<param name="connection">a connection to be analyzed</param>
+		<param name="node">current node</param>
+		<returns> a neigbhouring node </returns>
+		**/
+		public static NodeUI GetNeighbourNode(ConnectionUI connection, NodeUI node)
+		{
+			if (connection.connectedNodeA.GetID() != node.GetID())
+			{
+				return connection.connectedNodeA;
+			}
+			return connection.connectedNodeB;
+		}
+		/**
+		<summary> 
+		<para>Function, which returns an list of active neighbouring nodes</para>
+		</summary>
+		<returns> a list of active neighbouring nodes </returns>
+		**/
+		public List<ConnectionUI> GetActiveNeigbhourNodeList()
+		{
+			List<ConnectionUI> activeNeighbours = new();
+			foreach (var conn in connections)
+			{
+				NodeUI neigbhour = GetNeighbourNode(conn);
+				bool isEmpty = neigbhour.type == NodeType.Empty;
+
+				if (neigbhour.active || isEmpty)
+				{
+					activeNeighbours.Add(conn);
+				}
+			}
+			return activeNeighbours;
+		}
+
+		/**
+		<summary> 
+		<para>Function, which sets the location of then node</para>
+		</summary>
+		<returns> a list of active neighbouring nodes </returns>
+		**/
+		public void SetLocation(Vector2 location)
+		{
+			this.location = location;
+		}
+
+		/**
+		<summary> 
+		<para>Function, which returns the location of the node</para>
+		</summary>
+		<returns> location of the node </returns>
+		**/
+		public Vector2 GetLocation()
+		{
+			return location;
+		}
+		/**
+		<summary> 
+		<para>Function, which sets the node to be active</para>
+		</summary>
+		**/
+		public void SetActive()
+		{
+			node_image.SetImage(active_node_image);
+			active = true;
+		}
+		/**
+		<summary> 
+		<para>Function, which sets the node to be inactive</para>
+		</summary>
+		**/
+		public void SetInActive()
+		{
+			node_image.SetImage(inactive_node_image);
+			active = false;
+		}
+		/**
+		<summary> 
+		<para>Function, which edits the hover over element to display right information</para>
+		</summary>
+		**/
 		public void EditHoverOverElement()
 		{
 			hoverOverUI.ChangeDescription(description);
@@ -239,49 +328,113 @@ namespace Runeforge.Content.UI
 			}
 		}
 
-		public override void MouseOver(UIMouseEvent evt)
-		{
-			base.MouseOver(evt);
-			if (!mainPanel.isDragging)
-			{
-				mainPanel.isHoveringOverUI = true;
-			}
-			if (type != NodeType.Empty)
-			{
-				EditHoverOverElement();
-			}
-		}
-		public override void MouseOut(UIMouseEvent evt)
-		{
-			base.MouseOver(evt);
-			if (!mainPanel.isDragging)
-			{
-				mainPanel.isHoveringOverUI = false;
-			}
-			hoverOverUI.SetTransparent();
-			hoverOverUI.ChangeDescription("");
-		}
-
-		public override void Update(GameTime gameTime)
-		{
-			base.Update(gameTime);
-			Width.Set(node_image.Width.Pixels * SkillTreePanel.zoom, 0f);
-			Height.Set(node_image.Height.Pixels * SkillTreePanel.zoom, 0f);
-			location = basePosition * SkillTreePanel.zoom + SkillTreePanel.panOffset;
-			Top.Set(location.Y, 0f);
-			Left.Set(location.X, 0f);
-			Recalculate();
-		}
-
 		public override int GetHashCode()
 		{
 			return node_image.GetHashCode();
 		}
-
-        public int GetID() { return id; }
-        public NodeType GetNodeType() { return type; }
-        public List<ConnectionUI> GetConnections() { return connections; }
-        public INodeTrigger GetModification() { return trigger; }
-        public void AddConnection(ConnectionUI conn) { connections.Add(conn); }
+		public override string ToString()
+		{
+			//                                             NodeID | Type   | Connections                     | Description
+			// to string method generates a string such as NodeID | TypeID | {(NodeID, NodeID, active), ...} | description
+			StringBuilder nodeString = new StringBuilder();
+			nodeString.Append($"{id} | {type} | ");
+			nodeString.Append('{');
+			foreach (var conn in connections)
+			{
+				nodeString.Append(conn);
+				nodeString.Append(',');
+			}
+			nodeString.Append("} | ");
+			nodeString.Append($"{description}");
+			return nodeString.ToString();
+		}
+		/**
+		<summary> 
+		<para>Function, which gets node activity</para>
+		</summary>
+		**/
+		public bool GetNodeActivity()
+		{
+			return active;
+		}
+		/**
+		<summary> 
+		<para>Function, which gets node's linked statblock</para>
+		</summary>
+		**/
+		public StatBlock GetStatBlock()
+		{
+			return statBlock;
+		}
+		/**
+		<summary> 
+		<para>Function, which sets node's linked statblock</para>
+		</summary>
+		<param name="statBlock"> statblock to be set to this node </param>
+		**/
+		public void SetStatBlock(StatBlock statBlock)
+		{
+			this.statBlock = statBlock;
+		}
+		/**
+		<summary> 
+		<para>Function, which returns the description of the node</para>
+		</summary>
+		<returns> a string, which describes the functionality of the node </returns>
+		**/
+		public string GetDescription()
+		{
+			return description;
+		}
+		/**
+		<summary> 
+		<para>Function, which returns the id of the node</para>
+		</summary>
+		<returns> unique id of the node </returns>
+		**/
+		public int GetID()
+		{
+			return id;
+		}
+		/**
+		<summary> 
+		<para>Function, which returns the type of the node</para>
+		</summary>
+		<returns> returns the type of the node </returns>
+		**/
+		public NodeType GetNodeType()
+		{
+			return type;
+		}
+		/**
+		<summary> 
+		<para>Function, which returns the list of connections of the node</para>
+		</summary>
+		<returns> returns the list of connections </returns>
+		**/
+		public List<ConnectionUI> GetConnections()
+		{
+			return connections;
+		}
+		/**
+		<summary> 
+		<para>Function, which returns the trigger of the node</para>
+		</summary>
+		<returns> returns node's trigger </returns>
+		**/
+		public INodeTrigger GetModification()
+		{
+			return trigger;
+		}
+		/**
+		<summary> 
+		<para>Function, which adds a connectio to the node</para>
+		</summary>
+		<param name="conn"> connection to be added to list of connections of this node</param>
+		**/
+		public void AddConnection(ConnectionUI conn)
+		{
+			connections.Add(conn);
+		}
 	}
 }
