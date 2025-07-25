a10cee536560e7ff103f0fc8197ceb76378f7716
VK <ville.koli.wr@gmail.com>
2025-07-25 17:05:51 +0300

    added documentation to HoverOverUI

--- Content/UI/HoverOverUI.cs
+++ Content/UI/HoverOverUI.cs
@@ -4,37 +4,35 @@ using Terraria.UI;
 using Terraria.GameContent.UI.Elements;
 using Runeforge.Content.SkillTree;
 using ReLogic.Content;
+using Terraria;
 
 namespace Runeforge.Content.UI
 {
 	public class HoverOverUI : UIElement
 	{
 		public UIImage hoverOverUIImage;
-        public ConnectionDirection direction_from_node;
 		public Asset<Texture2D> activeConnectionImage;
 		public Asset<Texture2D> inActiveConnectionImage;
 		public Asset<Texture2D> transparentImage;
 		public UIText description;
 		public bool active;
-		private int id;
-		private static int global_id = 0;
 		public HoverOverUI(TextureManager textureManager)
 		{
 			transparentImage = textureManager.transparent_box;
 			(Asset<Texture2D> active, Asset<Texture2D> inactive) asset = textureManager.GetUI(UIType.hoverOver);
 			hoverOverUIImage = new UIImage(transparentImage);
-			this.description = new UIText("test", 0.7f); // DO NOT SET "test" to "" when changing font size, if font size is 1 then it can be ""
+			description = new UIText("test", 0.7f); // DO NOT SET "test" to "" when changing font size, if font size is 1 then it can be ""
 			inActiveConnectionImage = asset.inactive;
 			activeConnectionImage = asset.active;
-			id = global_id;
-			global_id++;
 		}
 
-		public int GetID()
-		{
-			return id;
-		}
+		/**
+		<summary>
+		Changes the description of the hover over ui
+		</summary>
 
+		<returns> returns a boolean if new description was set or not  </returns>
+		**/
 		public bool ChangeDescription(string newDescription)
 		{
 			if (description != null)
@@ -45,27 +43,48 @@ namespace Runeforge.Content.UI
 			return false;
 		}
 
+		/**
+		<summary>
+		Changes hover over ui to be active
+		</summary>
+		**/
 		public void SetActive()
 		{
 			hoverOverUIImage.SetImage(activeConnectionImage);
 			active = true;
 		}
+
+		/**
+		<summary>
+		Changes hover over ui to be inactive
+		</summary>
+		**/
 		public void SetInActive()
 		{
 			hoverOverUIImage.SetImage(inActiveConnectionImage);
 			active = false;
 		}
 
+		/**
+		<summary>
+		Changes hover over ui to be invisible
+		</summary>
+		**/
 		public void SetTransparent()
 		{
 			hoverOverUIImage.SetImage(transparentImage);
 		}
 
+		/**
+		<summary>
+		Initializes the hover over ui element
+		</summary>
+		**/
 		public override void OnInitialize()
 		{
 			base.OnInitialize();
-			Width.Set(50, 0f);
-			Height.Set(50, 0f);
+			Width.Set(inActiveConnectionImage.Width(), 0f);
+			Height.Set(inActiveConnectionImage.Height(), 0f);
 			description.PaddingTop = 15;
 			description.PaddingLeft = 15;
 			description.SetText("");
@@ -73,6 +92,11 @@ namespace Runeforge.Content.UI
 			hoverOverUIImage.Append(description);
 		}
 
+		/**
+		<summary>
+		Changes hover over ui element in-game tick while ui is open
+		</summary>
+		**/
 		public override void Update(GameTime gameTime)
 		{
 			base.Update(gameTime);
